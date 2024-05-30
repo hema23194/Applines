9580f196c0afb63f6a814ea4568759e13593b173
Hema pantham <hema.pantham@accelyzei.com>
2024-05-30 15:53:59 +0530

    Changes on App.js file for viewing visa cad layout

--- Hotstar/hotstar/src/App.js
+++ Hotstar/hotstar/src/App.js
@@ -1,7 +1,7 @@
 // import Home from './components/Home';
 // import Home from './dj Hype/Home';
-// import Home from "./VISA_Card/Home";
-import Home from "./WebCam/Home";
+import Home from "./VISA_Card/Home";
+// import Home from "./WebCam/Home";
 
 function App() {
   return (
